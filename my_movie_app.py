bb810cc062f549d1f90d56d1648d167e0dfc95f4
AmandineFournie <afournie001@gmail.com>
2023-06-08 11:04:32 +0200

    modified:   my_movie_app.py

--- my_movie_app.py
+++ my_movie_app.py
@@ -151,7 +151,50 @@ elif selected == '✨ Présentation ✨':
 
 # KPI via PowerBi
 elif selected == '📈 PowerBI 📈':
+    IMAGES = [
+        "https://www.obs-ed.fr/wp-content/uploads/2023/06/Movies1.png",
+        "https://www.obs-ed.fr/wp-content/uploads/2023/06/Movies2.png",
+        "https://www.obs-ed.fr/wp-content/uploads/2023/06/Movies3.png",
+    ]
 
+    def slideshow_swipeable(images):
+        key = f"slideshow_swipeable_{str(images).encode().hex()}"
+        if key not in st.session_state:
+            st.session_state[key] = 0
+
+        index = st.session_state[key]
+        with elements(f"frame_{key}"):
+
+            # Use mui.Stack to vertically display the slideshow and the pagination centered.
+            # https://mui.com/material-ui/react-stack/#usage
+            with mui.Stack(spacing=2, alignItems="center"):
+
+                # Create a swipeable view that updates st.session_state[key] thanks to sync().
+                # It also sets the index so that changing the pagination (see below) will also
+                # update the swipeable view.
+                # https://mui.com/material-ui/react-tabs/#full-width
+                # https://react-swipeable-views.com/demos/demos/
+                with mui.SwipeableViews(index=index, resistance=True, onChangeIndex=sync(key)):
+                    for image in images:
+                        html.img(src=image, css={"width": "100%"})
+
+                # Create a handler for mui.Pagination.
+                # https://mui.com/material-ui/react-pagination/#controlled-pagination
+                def handle_change(event, value):
+                    # Pagination starts at 1, but our index starts at 0, explaining the '-1'.
+                    st.session_state[key] = value-1
+
+                # Display the pagination.
+                # As the index value can also be updated by the swipeable view, we explicitely
+                # set the page value to index+1 (page value starts at 1).
+                # https://mui.com/material-ui/react-pagination/#controlled-pagination
+                mui.Pagination(page=index+1, count=len(images), color="primary", onChange=handle_change)
+
+
+    if __name__ == '__main__':
+
+    
+        slideshow_swipeable(IMAGES)    
     st.markdown('<iframe title="my_movies" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=7f5a7697-c1ca-46ff-b313-8fe2804b4504&autoAuth=true&ctid=5892e2db-e39d-4cc1-a179-dc66550efc30" frameborder="0" allowfullscreen></iframe>', unsafe_allow_html=True)
 
 # Système de recommendation
